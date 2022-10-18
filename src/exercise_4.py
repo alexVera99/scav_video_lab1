f2cca21d22f32d7881adfc02ef8adbae0eb03273
alexVera99 <alexanderjoel.vera99@gmail.com>
2022-10-18 21:41:52 +0200

    update: change the function to process string byte sequences

--- src/exercise_4.py
+++ src/exercise_4.py
@@ -1,4 +1,14 @@
-def run_length_code_sequence(sequence: str):
+def run_length_code_str_byte_sequence(sequence: bytes,
+                                      encoding: str = "utf-8") -> str:
+    """
+    Apply run-length encoding to the given byte sequence
+    :param sequence: Byte string sequence to apply the run-length encoding\
+    :param encoding: encoding type. default: utf-8
+    per character
+    :return: string representing the code from the run-length encoding
+    """
+    # Decode the sequence
+    sequence = sequence.decode(encoding)
 
     previous_char = sequence[0]
     counter = 0
@@ -21,12 +31,22 @@ def run_length_code_sequence(sequence: str):
     # Add the last character occurrences
     occurrences.append([previous_char, counter])
 
-    print(occurrences)
+    # Compute the final string
+    code = ''.join([f"{_c}{_o}" for _c, _o in occurrences])
+
+    return bytes(code, encoding)
 
 
 def main():
-    st = "wwwwaaadexxxxxxywww"
-    run_length_code_sequence(st)
+    """
+    Show an example of how to use the constructed functions.
+    :return: no return
+    """
+    st = b"wwwwaaadexxxxxxywww"
+
+    code = run_length_code_str_byte_sequence(st)
+
+    print(code)
 
 
 if __name__ == "__main__":
